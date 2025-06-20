f92df71dadfe98ea5d80b10d7e78a9b812b1c4e6
vtllms <vt.llms.lab@gmail.com>
2025-06-20 21:46:30 +0000

    Deploying to gh-pages from @ vtllms/vtllms.github.io@98f69522b37f3c199d166a1c1f5fdea12556477f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,7 +430,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "people-tu-vu",
+          title: 'Tu Vu',
+          description: "Assistant professor",
+          section: "People",handler: () => {
+              window.location.href = "/people/tu/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
