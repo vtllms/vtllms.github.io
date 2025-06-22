24d8f5693c16d4b8c72eeeb0dbd3bbde9fc3a73b
vtllms <vt.llms.lab@gmail.com>
2025-06-22 20:14:59 +0000

    Deploying to gh-pages from @ vtllms/vtllms.github.io@5dd29636dc35368cb7ecdd1471b41ef6adec7883 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,21 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "dropdown-bookshelf",
+              title: "bookshelf",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/books/";
+              },
+            },{id: "dropdown-blog",
+              title: "blog",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/blog/";
+              },
+            },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
