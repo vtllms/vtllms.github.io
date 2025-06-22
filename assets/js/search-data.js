df91f66fff4113e0cb0fb997ca98c95f5fe50444
vtllms <vt.llms.lab@gmail.com>
2025-06-22 20:24:36 +0000

    Deploying to gh-pages from @ vtllms/vtllms.github.io@eefa456a26976add7f5c0bb2f12e945bbd9672e8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-people",
+          title: "people",
+          description: "Members of the lab",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
+        },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
